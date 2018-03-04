9f19d47a7cf6668baf5a44b0b513ceb277a6129a
Mark Lewis <mlewis@trinity.edu>
2018-03-04 06:58:51 -0600

    Class changes from 3/1/2018.

--- app/controllers/TempController.scala
+++ app/controllers/TempController.scala
@@ -5,10 +5,21 @@ import play.api.mvc.ControllerComponents
 import javax.inject._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.validation.Constraints._
+
+case class TempRange(startMonth: Int, startYear: Int, endMonth: Int, endYear: Int)
 
 @Singleton
 class TempController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
   val td = new models.TempData("data/SanAntonioTemps.csv")
+
+  val tempRangeForm = Form(mapping(
+    "startMonth" -> number(min = 1, max = 12),
+    "startYear" -> number(min = 1946, max = 2014),
+    "endMonth" -> number(min = 1, max = 12),
+    "endYear" -> number(min = 1946, max = 2014))(TempRange.apply)(TempRange.unapply))
 
   def welcome = Action { implicit request =>
     println(request.session)
@@ -21,15 +32,18 @@ class TempController @Inject() (cc: ControllerComponents) extends AbstractContro
         val month = formData("month").head.toInt
         val year = formData("year").head.toInt
         val data = td.getMonth(month, year)
-    	  Ok(views.html.tempTable(data, month, year))
+        Ok(views.html.tempTable(data, month, year))
       case None =>
         Redirect(routes.TempController.welcome)
     }
   }
 
-  def tempPlot(month: Int, year: Int) = Action.async { implicit request =>
+  def tempPlot = Action.async { implicit request =>
     Future {
-      Ok("Requested plot " + month + "/" + year)
+      tempRangeForm.bindFromRequest().fold(
+          formWithErrors => BadRequest,
+          tempRange => 
+            Ok("Requested plot " + tempRange))
     }
   }
 
